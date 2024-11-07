03e1d614e3d196ae23ee570b9bd5e60fbd9a5495
OhadZvi <ohad.zvi@post.runi.ac.il>
2024-11-07 19:16:49 +0200

    completed FVCalc task

--- FVCalc.java
+++ FVCalc.java
@@ -5,7 +5,7 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		double futureValue = currentValue * Math.pow(1 + rate/100 , n);
-		System.out.println("After " + n + " years, $" + currentValue +
+		System.out.println("After " + n + " years, a $" + currentValue +
 							" saved at " + rate + "% will yield $" + (int)futureValue);
 
 	}
